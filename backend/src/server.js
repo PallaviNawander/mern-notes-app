367afbd4284b5d6440838f567e2c283a0695664d
PallaviNawander <pallavinawander@gmail.com>
2026-08-30 02:29:26 +0530

    prepared for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -3,16 +3,22 @@ import notesRoutes from "./routes/notesRoutes.js"
 import {connectDB} from "./config/db.js"
 import dotenv from "dotenv"
 import rateLimiter from "./middleware/rateLimiter.js";
+import path from "path";
 //const express = require("express")
 import cors from "cors"
 dotenv.config();
 const app=express();
 const PORT=process.env.PORT || 5001
+
+const __dirname = path.resolve()
 //middleware
-app.use(cors({
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
     origin:"http://localhost:5173",
 }
 ));
+}
+
 app.use(express.json()); 
 app.use(rateLimiter);// this middleware will parse JSON bodies : req.body
 /*app.use((req,res,next)=>{
@@ -21,6 +27,13 @@ app.use(rateLimiter);// this middleware will parse JSON bodies : req.body
 }); */// our simple custom middleware
 
 app.use("/api/notes",notesRoutes);
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+    app.get("*",(req,res)=>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    });
+}
 connectDB().then(()=>{
     app.listen(PORT, ()=>{
        console.log("Server started on PORT : 5001");
